763da4f4b50d661310350808ae3b7795aeea70c5
BaronRouge-sudo <carlos.resa88@gmail.com>
2025-01-13 10:19:23 +0000

    Entrega

--- src/app.py
+++ src/app.py
@@ -1,18 +1,17 @@
+# Ejemplo de lectura de la base de datos SQL desde aquí
 import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 import pandas as pd
-from dotenv import load_dotenv
 
-# load the .env file variables
+# cargar las variables del archivo .env
 load_dotenv()
-
-# 1) Connect to the database here using the SQLAlchemy's create_engine function
 
 connection_string = f"postgresql://{os.getenv('gitpod')}:{os.getenv('postgres')}@{os.getenv('localhost')}/{os.getenv('sample_db')}"
 engine = create_engine(connection_string).execution_options(autocommit=True)
 engine.connect()
 
-# 2) Execute the SQL sentences to create your tables using the SQLAlchemy's execute function
+# Creacion de tablas (create.sql)
 engine.execute(""" CREATE TABLE publishers(
     publisher_id INT NOT NULL,
     name VARCHAR(255) NOT NULL,
@@ -48,7 +47,7 @@ CREATE TABLE book_authors (
 );  """)
 
 
-# 3) Execute the SQL sentences to insert your data using the SQLAlchemy's execute function
+# Introducción de registros (insert.sql)
 engine.execute("""  -- publishers
 INSERT INTO publishers(publisher_id, name) VALUES (1, 'O Reilly Media');
 INSERT INTO publishers(publisher_id, name) VALUES (2, 'A Book Apart');
@@ -93,6 +92,6 @@ INSERT INTO book_authors (book_id, author_id) VALUES (9, 4);
 INSERT INTO book_authors (book_id, author_id) VALUES (10, 1);   """)
 
 
-# 4) Use pandas to print one of the tables as dataframes using read_sql function
+# Mostrar mediante Pandas
 dataframe = pd.read_sql("Select * from books;", engine)
 print(dataframe.describe())
